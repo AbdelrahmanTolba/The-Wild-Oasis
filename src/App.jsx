3fbf7d7d30347519b155c60cfe12317a7bb35105
AbdelrahmanTolba <abdelrahman.ibrahim83@gmail.com>
2023-12-12 03:12:53 +0200

    Add Protected route for Auth.

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 //DATABASE PASSWORD: KIkrzC19FYQJVrWS
 
@@ -35,7 +36,13 @@ export default function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
